1188752183dc6d51f6c6e2df0d7a2d59322bc1fb
yfaheid <131709066+yfaheid@users.noreply.github.com>
2026-05-07 10:57:43 -0500

    add custom readiness handler

--- main.go
+++ main.go
@@ -4,7 +4,14 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 	server := http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
